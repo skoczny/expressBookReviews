c7f6fd39ef3d2770bb99e471f84186d8097caef2
skoczny <kacper.skoneczny@o2.pl>
2026-04-07 11:41:47 -0400

    Task 12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -55,21 +55,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-    const author = req.params.author
+public_users.get('/author/:author', async function (req, res) {
+    try {
+        const author = req.params.author;
+        const allKeys = Object.keys(books);
+        const booksByAuthor = [];
 
-    const allKeys = Object.keys(books)
+        allKeys.forEach(key => {
+            if (books[key].author === author) {
+                booksByAuthor.push(books[key]);
+            }
+        });
 
-    const booksbyAuthor =[]
-
-    allKeys.forEach(key => {
-        if(books[key].author === author){
-            booksbyAuthor.push(books[key])
-        }
-    })
-
-    res.send(JSON.stringify(booksbyAuthor, null, 4))
+        res.send(JSON.stringify(booksByAuthor, null, 4));
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching books by author", error: error.message });
+    }
 });
 
 // Get all books based on title
